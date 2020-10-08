ca9cec4d812acdfbff89f9e02157bbd9565aa17f
Unathi Skosana <ukskosana@gmail.com>
2020-10-08 11:00:28 +0200

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tint
-Matplotlib stylesheet
+# Mint Matplotlib stylesheet (WIP)
+
+Matplotlib stylesheet inspired by [Tint](https://eddelbuettel.github.io/tint/)
+
+```python
+plt.style.use('https://raw.githubusercontent.com/Unathi-Skosana/mint/main/tint.mplstyle')
+```
+
+## demos
+
+![bar3d](https://raw.githubusercontent.com/unathi-skosana/mint/main/img/bar3d.png "bar3d")
+
+![contour3d](https://raw.githubusercontent.com/unathi-skosana/mint/main/img/contour3d.png "contour3d")
+
+![misc1.png](https://raw.githubusercontent.com/unathi-skosana/mint/main/img/misc1.png "misc1")
+
+![misc2.png](https://raw.githubusercontent.com/unathi-skosana/mint/main/img/misc2.png "misc2")
--- demos.py
+++ demos.py
@@ -1,20 +1,38 @@
 if __name__ == '__main__':
-
     # This import registers the 3D projection, but is otherwise unused.
-    from mpl_toolkits.mplot3d import Axes3D  # nova: Fair unused import
-    from mpl_toolkits.mplot3d import axes3d
+
+    from mpl_toolkits.mplot3d import Axes3D, axes3d # nova: Fair unused import
+    from matplotlib.colors import ListedColormap, \
+        to_rgba_array
     from cycler import cycler
     from numpy.random import beta
 
     import matplotlib.pyplot as plt
-    import matplotlib.cm as cm
     import numpy as np
-    
-    plt.style.use('tint')
+
+    plt.style.use('./mint.mplstyle')
+
+    # create custom colormap from prop_cycle
+
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    N = 256
+    M = len(colors)
+    dx = N//M
+    vals = np.ones((N, 4))
+
+    for i in range(M):
+        start = i * dx
+        end = dx + i * dx
+        if i == M - 1:
+            vals[start:, :] = to_rgba_array(colors[i], 1.0)
+        else:
+            vals[start:end, :] = to_rgba_array(colors[i], 1.0)
+
+    cmap = ListedColormap(vals)
 
     # Fixing random state for reproducibility
     np.random.seed(19680801)
-
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -46,7 +64,6 @@ if __name__ == '__main__':
     dx = dy = 0.5 * np.ones_like(zpos)
     dz = hist.ravel()
 
-    cmap = cm.get_cmap('tab20_r')
     max_height = np.max(dz)   # get range of colorbars
     min_height = np.min(dz)
 
@@ -60,7 +77,6 @@ if __name__ == '__main__':
     ax.set_zlabel('Z')
 
     ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
-
 
     fig1 = plt.figure()
     ax1 = fig1.gca(projection='3d')
@@ -83,14 +99,14 @@ if __name__ == '__main__':
     X, Y, Z = axes3d.get_test_data(0.05)
 
     # Plot the 3D surface
-    ax1.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
+    ax1.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.5)
 
     # Plot projections of the contours for each dimension.  By choosing offsets
     # that match the appropriate axes limits, the projected contours will sit on
     # the 'walls' of the graph
-    cset = ax1.contour(X, Y, Z, zdir='z', offset=-100)
-    cset = ax1.contour(X, Y, Z, zdir='x', offset=-40)
-    cset = ax1.contour(X, Y, Z, zdir='y', offset=40)
+    cset = ax1.contour(X, Y, Z, zdir='z', offset=-100, cmap=cmap)
+    cset = ax1.contour(X, Y, Z, zdir='x', offset=-40, cmap=cmap)
+    cset = ax1.contour(X, Y, Z, zdir='y', offset=40, cmap=cmap)
 
     ax1.set_xlim(-40, 40)
     ax1.set_ylim(-40, 40)
@@ -100,7 +116,6 @@ if __name__ == '__main__':
     ax1.set_ylabel('Y')
     ax1.set_zlabel('Z')
 
-
     fig, axes = plt.subplots(ncols=2, nrows=3)
     ax2, ax3, ax4, ax5, ax6, ax7 = axes.ravel()
 
@@ -109,7 +124,6 @@ if __name__ == '__main__':
     def plot_beta_hist(ax, a, b):
         ax.hist(beta(a, b, size=10000), histtype="stepfilled",
                 bins=25, alpha=0.8, density=True)
-
 
     plot_beta_hist(ax2, 10, 10)
     plot_beta_hist(ax2, 4, 12)
@@ -125,10 +139,7 @@ if __name__ == '__main__':
                           connectionstyle="angle,angleA=-95,angleB=35,rad=10"),
                 )
 
-
-
-
-    # sin waves
+    # weierstrass function look-alike
 
     x = np.linspace(0, 10)
 
@@ -157,7 +168,8 @@ if __name__ == '__main__':
     ax5.plot(x, y, 'o')
 
 
-    # sinusoidal
+    # sinusoidal waves
+
     L = 2*np.pi
     x = np.linspace(0, L)
     ncolors = len(plt.rcParams['axes.prop_cycle'])
@@ -168,13 +180,17 @@ if __name__ == '__main__':
 
 
     # circles with colors from default color cycle
+
     for i, color in enumerate(plt.rcParams['axes.prop_cycle']):
         xy = np.random.normal(size=2)
         ax7.add_patch(plt.Circle(xy, radius=0.3, color=color['color']))
     ax7.axis('equal')
     ax7.margins(0)
 
-    # box plot
+    # box plot & fill between
+
+    fig8, axs = plt.subplots(2, 1)
+    ax8, ax9 = axs.ravel()
 
     spread = np.random.rand(50) * 100
     center = np.ones(25) * 50
@@ -182,35 +198,12 @@ if __name__ == '__main__':
     flier_low = np.random.rand(10) * -100
     data = np.concatenate((spread, center, flier_high, flier_low))
 
-    fig8, axs = plt.subplots(2, 3)
-
-    # basic plot
-    axs[0, 0].boxplot(data)
-
-    # notched plot
-    axs[0, 1].boxplot(data, 1)
-
-    # change outlier point symbols
-    axs[0, 2].boxplot(data, 0, 'D')
-
-    # don't show outlier points
-    axs[1, 0].boxplot(data, 0, '')
-
-    # horizontal boxes
-    axs[1, 1].boxplot(data, 0, 's', 0)
-
-    # change whisker length
-    axs[1, 2].boxplot(data, 0, 's', 0, 0.75)
-
-    fig8.subplots_adjust(left=0.08, right=0.98, bottom=0.05, top=0.9,
-                    hspace=0.4, wspace=0)
-
+    ax8.boxplot(data)
 
     x = np.arange(-5, 5, 0.01)
     y1 = -5*x*x + x + 10
     y2 = 5*x*x + x
 
-    fig9, ax9 = plt.subplots()
     ax9.plot(x, y1, x, y2, color='black')
     ax9.fill_between(x, y1, y2, where=y2 >y1, alpha=0.5, edgecolor='white')
     ax9.fill_between(x, y1, y2, where=y2 <=y1, alpha=0.5, edgecolor='white')
